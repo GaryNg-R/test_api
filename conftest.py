786f35fffd3f611afd718272dc4b8cffb3e71713
Gary Ng <garyng@garys-mbp.attlocal.net>
2020-06-25 22:28:40 -0400

    setup project

---
+++ conftest.py
@@ -0,0 +1,5 @@
+import os
+import sys 
+
+here = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(os.path.join(here, 'api'))
---
+++ test/test_unit.py
@@ -0,0 +1,8 @@
+import sys
+import pytest
+
+import call
+
+@pytest.mark.unit
+def test_base():
+    call.abc()
